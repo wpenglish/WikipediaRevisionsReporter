617250170610e89bb972ae4b6af94ccfac8b63fc
wpenglish <wpenglish27@gmail.com>
2021-04-21 23:44:50 -0400

    Removed throwing IOException

--- src/main/java/edu/bsu/cs/model/Formatter.java
+++ src/main/java/edu/bsu/cs/model/Formatter.java
@@ -3,5 +3,5 @@ package edu.bsu.cs.model;
 import java.io.IOException;
 
 public interface Formatter {
-    String format(Revision revision) throws IOException;
+    String format(Revision revision);
 }

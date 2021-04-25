b9809a28c1cf331299a1164d2bed8d1f0dd06ddb
wpenglish <wpenglish27@gmail.com>
2021-04-25 19:06:54 -0400

    Updated the revision formatted output to be more readable

--- src/main/java/edu/bsu/cs/model/RevisionFormatter.java
+++ src/main/java/edu/bsu/cs/model/RevisionFormatter.java
@@ -2,15 +2,18 @@ package edu.bsu.cs.model;
 
 import edu.bsu.cs.model.Revision;
 
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 public final class RevisionFormatter implements Formatter {
 
     @Override
     public String format(Revision revision) {
-        return String.format("%s made a change at %s",
+        String[] localDateTime = LocalDateTime.ofInstant(revision.timestamp, ZoneOffset.UTC).toString().split("T");
+        return String.format("%s made a change on %s at %s",
                 revision.name,
-                DateTimeFormatter.ISO_INSTANT.format(revision.timestamp));
+                localDateTime[0],
+                localDateTime[1]);
     }
-    
+
 }
